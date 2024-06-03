2e713ef62efe60d4c7ac065d646494fd5f0d9887
cony-design <takeo.kotani@gmail.com>
2024-06-03 17:49:08 +0900

    作ったものをコミットする

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import { getArticles } from '@/lib/newt'
+import styles from '@/app/page.module.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Newt・Next.jsブログ',
+  description: 'NewtとNext.jsを利用したブログです',
+}
+
+export default async function Home() {
+  const articles = await getArticles()
+  return (
+    <main className={styles.main}>
+      <ul>
+        {articles.map((article) => {
+          return (
+            <li key={article._id}>
+              <Link href={`articles/${article.slug}`}>{article.title}</Link>
+            </li>
+          )
+        })}
+      </ul>
+    </main>
+  )
+}
